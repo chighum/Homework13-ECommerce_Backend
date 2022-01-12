f32788f31d821f53230cb24703d612feaf0a68ac
Charlie Highum <chighum41@gmail.com>
2022-01-12 12:09:44 -0700

    made table associations models/index.js

--- models/index.js
+++ models/index.js
@@ -1,16 +1,28 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "product_id",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
 
-// Products belongToMany Tags (through ProductTag)
+// // Products belongToMany Tags (through ProductTag)
+// Product.belongsToMany(ProductTag, {
+//   foreignKey: "product_id",
+// });
 
-// Tags belongToMany Products (through ProductTag)
+// // Tags belongToMany Products (through ProductTag)
+// Tag.belongsToMany(ProductTag, {
+//   foreignKey: "tag_id",
+// });
 
 module.exports = {
   Product,
